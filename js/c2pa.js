d34bafbea501fcb1f3a3267369a6ca01ede9777e
James R Glasgow <james.glasgow@associates.hq.dhs.gov>
2024-02-05 08:51:01 -0700

    Fixes #3 - Limiting the scop of 'once' to just the .c2pa-wrapper elements and the current context (the items added to the page)

--- js/c2pa.js
+++ js/c2pa.js
@@ -5,10 +5,11 @@ import {
 } from "https://cdn.jsdelivr.net/npm/c2pa@0.14.4/+esm";
 import "https://cdn.jsdelivr.net/npm/c2pa-wc@0.10.15/+esm";
 
-((Drupal, once) => {
+((Drupal, once, drupalSettings) => {
   Drupal.behaviors.c2pa = {
-    async attach() {
-      once("init-c2pa", "html").forEach(async (element) => {
+    async attach(context, settings) {
+      // only act once on the .c2pa-wrapper classed elements in the current context
+      once("init-c2pa", ".c2pa-wrapper", context).forEach(async (wrapper) => {
         const c2pa = await createC2pa({
           wasmSrc:
             "https://cdn.jsdelivr.net/npm/c2pa@0.14.4/dist/assets/wasm/toolkit_bg.wasm",
@@ -16,8 +17,8 @@ import "https://cdn.jsdelivr.net/npm/c2pa-wc@0.10.15/+esm";
             "https://cdn.jsdelivr.net/npm/c2pa@0.14.4/dist/c2pa.worker.min.js",
         });
 
-        element.querySelectorAll(".c2pa-wrapper").forEach(async (wrapper) => {
-          const image = wrapper.querySelector("img");
+        // within the .c2pa-wrapper find only img elements
+        wrapper.querySelectorAll("img").forEach(async (image) => {
           const result = await c2pa.read(image);
           const manifestStoreResult = await createL2ManifestStore(
             result.manifestStore
@@ -45,4 +46,4 @@ import "https://cdn.jsdelivr.net/npm/c2pa-wc@0.10.15/+esm";
       });
     },
   };
-})(Drupal, once);
+})(Drupal, once, drupalSettings);
